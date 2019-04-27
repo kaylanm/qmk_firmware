e9eb662c4e6fcd867c61432bc0ce30cc0e2cdd94
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2019-04-26 23:58:16 -0400

    Fix PC keymap.

--- keyboards/singa/keymaps/kaylanm/keymap.c
+++ keyboards/singa/keymaps/kaylanm/keymap.c
@@ -19,10 +19,6 @@
 // Helpful defines
 #define _______ KC_TRNS
 
-#define KC_OSX_EJECT 0x66
-#define LOCK_OSX LSFT(LCTL(KC_OSX_EJECT))
-#define SLEEP_OSX LALT(LGUI(KC_OSX_EJECT))
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: Qwerty layer
    * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -42,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 0: ANSI qwerty */
   [0] = LAYOUT_wk(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_F10,   KC_F11,   KC_F12,   MO(1),  KC_INS,   KC_DEL, \
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_F10,   KC_F11,   KC_F12,   MO(1),  KC_INS,     KC_DEL, \
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_HOME,  \
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,  \
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_PGDN, \
@@ -67,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 3: ANSI Fn layer */
   [1] = LAYOUT_wk(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_F7,    KC_F8,    KC_F9,    _______,  _______,  _______,  _______,  _______,  SLEEP_OSX, \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC__MUTE, \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC__VOLUP, \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_MOD,  RGB_VAI,  RGB_VAD,  _______,            _______,            KC__VOLDOWN, \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_F7,    KC_F8,    KC_F9,    _______,  _______,  _______,  _______,  _______,  KC_SLEP, \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_MUTE, \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_VOLU, \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_MOD,  RGB_VAI,  RGB_VAD,  _______,            _______,            KC_VOLD, \
     _______,  _______,  BL_TOGG,  BL_INC,   BL_DEC,   BL_BRTG,  RGB_TOG,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,                      _______,  _______,  _______, \
     _______,  _______,  _______,                                RESET  ,                                _______,  _______,            _______,  _______,  _______),
 
