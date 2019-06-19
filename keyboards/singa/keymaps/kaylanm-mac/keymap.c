891c63799e19971737d829916a980165dd725be3
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2019-06-19 17:01:09 -0400

    Add mply.

--- keyboards/singa/keymaps/kaylanm-mac/keymap.c
+++ keyboards/singa/keymaps/kaylanm-mac/keymap.c
@@ -26,7 +26,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: Qwerty layer
    * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   * │ ESC │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │  FN │ INS │ DEL │
+   * │ ESC │ F1  │ F2  │ F3  │ F4  │ F5  │MPLY │MUTE │VOL+ │VOL- │ F10 │ F11 │ F12 │  FN │ INS │ DEL │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    * │  `  │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │  -  │  =  │▒▒▒▒▒|BKSPC│HOME │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 0: ANSI qwerty */
   [0] = LAYOUT_wkl(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_F10,   KC_F11,   KC_F12,   MO(1),  KC_INS,   KC_DEL, \
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_MPLY,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_F10,   KC_F11,   KC_F12,   MO(1),  KC_INS,   KC_DEL, \
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_HOME,  \
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,  \
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_PGDN, \
