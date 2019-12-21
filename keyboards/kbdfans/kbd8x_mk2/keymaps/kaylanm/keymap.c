7c9c8a9ac559bd16c07750908e19cc0ebfea2f90
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2019-12-21 13:24:28 -0500

    Switch AG_SWAP to AG_TOGG.

--- keyboards/kbdfans/kbd8x_mk2/keymaps/kaylanm/keymap.c
+++ keyboards/kbdfans/kbd8x_mk2/keymaps/kaylanm/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT( /* Function */
 
-	RESET,            AG_SWAP, _______, _______, _______,      _______, _______, _______, _______,     _______, _______, _______, _______,          KC_SLEP, _______, _______, \
+	RESET,            AG_TOGG, _______, _______, _______,      _______, _______, _______, _______,     _______, _______, _______, _______,          KC_SLEP, _______, _______, \
 	_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,          KC_MUTE, KC_VOLU, KC_MPLY, \
 	KC_CAPS, _______, KC_PGUP, _______, _______, _______, _______, _______, KC_UP,   _______, KC_MPRV, KC_MPLY, KC_MNXT,          _______,          KC_MPRV, KC_VOLD, KC_MNXT, \
 	_______, KC_HOME, KC_PGDN, KC_END,  _______, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   _______,                                     \
