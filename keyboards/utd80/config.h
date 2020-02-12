090c72efc12168ae9bc5244d38a3fe6a7bed94a1
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2020-02-11 22:56:12 -0500

    UTD80 VIA support.

--- keyboards/utd80/config.h
+++ keyboards/utd80/config.h
@@ -3,8 +3,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x360C
+#define PRODUCT_ID      0x360C
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    UTD
 #define PRODUCT         UTD80
