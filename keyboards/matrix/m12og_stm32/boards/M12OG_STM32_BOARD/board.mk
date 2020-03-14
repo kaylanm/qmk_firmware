2dd5b5c7fac500397a25406c2b85eb2ba28c396f
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2020-03-14 19:39:25 -0400

    Initial m12og_stm32 configuration.

---
+++ keyboards/matrix/m12og_stm32/boards/M12OG_STM32_BOARD/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(BOARD_PATH)/boards/M12OG_STM32_BOARD/board.c
+
+# Required include directories
+BOARDINC = $(BOARD_PATH)/boards/M12OG_STM32_BOARD
---
+++ keyboards/matrix/m12og_stm32/m12og_stm32.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "quantum.h"
+#include "action_layer.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include "hal.h"
+
+inline void m12og_board_led_1_on(void) { palSetPad(GPIOB, 7); }
+inline void m12og_board_led_2_on(void) { palSetPad(GPIOB, 5); }
+inline void m12og_board_led_1_off(void) { palClearPad(GPIOB, 7); }
+inline void m12og_board_led_2_off(void) { palClearPad(GPIOB, 5); }
+inline void m12og_led_all_off(void)
+{
+  palClearPad(GPIOB, 7);
+  palClearPad(GPIOB, 5);
+}
+
+void m12og_blink_all_leds(void);
+
+#define ____ KC_NO
+
+#define LAYOUT_tkl_ansi( \
+    K000,       K001, K002, K003, K004, K005, K006, K007, K008, K009, K00A, K00B, K00C,    K00D, K00E, K00F, \
+    K010, K011, K012, K013, K014, K015, K016, K017, K018, K019, K01A, K01B, K01C, K01D,    K03D, K01E, K01F, \
+    K020, K021, K022, K023, K024, K025, K026, K027, K028, K029, K02A, K02B, K02C, K02D,    K03E, K02E, K02F, \
+    K030, K031, K032, K033, K034, K035, K036, K037, K038, K039, K03A, K03B,       K03C,                      \
+    K040,       K041, K042, K043, K044, K045, K046, K047, K048, K049, K04A,       K04B,          K04C,       \
+    K050, K051, K052,                   K055,                         K056, K057, K058,    K04D, K04E, K03F  \
+) \
+{ \
+    { K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008, K009, K00A, K00B, K00C, K00D, K00E, K00F }, \
+    { K010,  K011,  K012,  K013,  K014,  K015,  K016,  K017,  K018, K019, K01A, K01B, K01C, K01D, K01E, K01F }, \
+    { K020,  K021,  K022,  K023,  K024,  K025,  K026,  K027,  K028, K029, K02A, K02B, K02C, K02D, K02E, K02F }, \
+    { K030,  K031,  K032,  K033,  K034,  K035,  K036,  K037,  K038, K039, K03A, K03B, K03C, K03D, K03E, K03F }, \
+    { K040,  K041,  K042,  K043,  K044,  K045,  K046,  K047,  K048, K049, K04A, K04B, K04C, K04D, K04E, ____ }, \
+    { K050,  K051,  K052,  ____,  ____,  K055,  K056,  K057,  K058, ____, ____, ____, ____, ____, ____, ____ }  \
+}
